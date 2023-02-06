4ec36050f7f6cfbf2d19f868df829824d507ed8f
ayosiray20 <ayosi20102000@gmail.com>
2023-02-07 00:58:42 +0530

    updated palindrome/prime

--- pallindrome.js
+++ pallindrome.js
@@ -1,3 +1,16 @@
 //
 //
 //
+let num=1221;
+let rev=0;
+
+while(num!=0){
+    rev=rev*10+(num%10);
+    num=Math.floor(num/10);
+}
+if(rev==num){
+    console.log("Palindrome");
+}
+else{
+    console.log("Not a palindrome");
+}
